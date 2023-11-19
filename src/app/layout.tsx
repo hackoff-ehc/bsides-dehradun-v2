b7d62b9188e077c48db139fa053399ab56aa7d70
iHarryD <anuraagvr555@gmail.com>
2023-11-19 17:34:05 +0530

    feat: add favicon and setup poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
-import type { Metadata } from "next";
-import "@/styles/globals.css";
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import '@/styles/globals.css';
+
+const poppins = Poppins({ weight: ['400', '500', '600', '700'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "BSides Dehradun",
-  description: "Official website of BSides Dehradun",
+  title: 'BSides Dehradun',
+  description: 'Official website of BSides Dehradun',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+      </head>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }

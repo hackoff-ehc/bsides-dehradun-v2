cbd97b55905a4462f3655a124407e076d9b0f2d9
Aachman <achmanagarwal7696@gmail.com>
2023-11-25 15:55:23 +0530

    Feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import {motion} from 'framer-motion'
 export default function Home() {
-  return <main>Hello</main>;
+  return <main>
+    <motion.h1
+    initial={{opacity: 0, y: -200}}
+    animate={{opacity: 1, y: 0}}
+    >
+      This is framer
+    </motion.h1>
+
+
+  </main>;
 }

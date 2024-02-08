dc5fb3a930d1d5a858973cd9a2113d3ced1add39
Harsheet Sharma <asarma626@gmail.com>
2024-02-08 23:32:24 +0530

    feat: Added the Code of Conduct Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,13 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        
+        
       },
+      fontFamily:{
+        'poppins': "var(--poppins)"
+      },
+      
     },
   },
   plugins: [],
